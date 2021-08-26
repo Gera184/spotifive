aff94c92e4301d4a06530e278f8e4104d71f79aa
Gera184 <oogeraoo159@gmail.com>
2021-08-26 14:59:15 +0300

    spotifive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import "react-toastify/dist/ReactToastify.css";
+import Song from "./components/song/Song";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+} from "react-router-dom";
+import Artist from "./components/artist/Artist";
+import Nav from "./components/nav/Nav";
+import Footer from "./components/footer/Footer";
 
-function App() {
+export default () => {
+  const [artists, setArtists] = useState([]);
+  const [query, setQuery] = useState("");
+  const [songIndex, setSongIndex] = useState();
+
+  useEffect(() => {
+    if (query) {
+      var options = {
+        method: "GET",
+        url: "https://genius.p.rapidapi.com/search",
+        params: { q: query },
+        headers: {
+          "x-rapidapi-host": "genius.p.rapidapi.com",
+          "x-rapidapi-key":
+            "9da9f853dcmsh86bba916aa656c2p18fc8djsne2562db22529",
+        },
+      };
+      axios
+        .request(options)
+        .then(function (response) {
+          setArtists(response.data.response.hits);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
+
+    setQuery("");
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundColor: "#222930",
+        overflowY: "hidden",
+        fontFamily: "Spartan sans-serif",
+      }}
+    >
+      <Nav />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Header artists={artists} setQuery={setQuery} />
+            <Main query={query} artists={artists} setSongIndex={setSongIndex} />
+          </Route>
+          <Route exact path="/home">
+            <Header artists={artists} setQuery={setQuery} />
+            <Main query={query} artists={artists} setSongIndex={setSongIndex} />
+          </Route>
+          <Route exact path="/song">
+            <Song songIndex={songIndex} artists={artists} />
+          </Route>
+          <Route exact path="/artist">
+            <Artist />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/song/Song.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { useLocation } from "react-router";
+import ReactPlayer from "react-player";
+import Loading from "../loading/Loading";
+
+export default function Song() {
+  toast.configure();
+  const [profile, setProfile] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const song = useLocation();
+  console.log(profile);
+  const notifyLoading = () => {
+    toast.warning("One second please", {
+      position: toast.POSITION.TOP_LEFT,
+    });
+  };
+  useEffect(() => {
+    if (!loading) {
+      notifyLoading();
+    }
+  }, [loading]);
+
+  useEffect(() => {
+    if (song.state.artist) {
+      var options = {
+        method: "GET",
+        url: `https://genius.p.rapidapi.com/songs/${song.state.artist.result.id}`,
+        headers: {
+          "x-rapidapi-host": "genius.p.rapidapi.com",
+          "x-rapidapi-key":
+            "9da9f853dcmsh86bba916aa656c2p18fc8djsne2562db22529",
+        },
+      };
+      axios
+        .request(options)
+        .then(function (response) {
+          setProfile(response.data.response.song);
+          setLoading(true);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    } else {
+      var options = {
+        method: "GET",
+        url: `https://genius.p.rapidapi.com/songs/${song.state.song.id}`,
+        headers: {
+          "x-rapidapi-host": "genius.p.rapidapi.com",
+          "x-rapidapi-key":
+            "9da9f853dcmsh86bba916aa656c2p18fc8djsne2562db22529",
+        },
+      };
+      axios
+        .request(options)
+        .then(function (response) {
+          setProfile(response.data.response.song);
+          setLoading(true);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
+  }, [song]);
+
+  if (profile) {
+    for (let index = 0; index < profile?.media?.length; index++) {
+      if (profile.media[index].provider === "youtube") {
+        var element = profile.media[index].url;
+      }
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <div className="container-fluid pt-5 ">
+          <div className="row pt-2">
+            <div class="col d-flex justify-content-center">
+              <div class="artist-box">
+                <img
+                  className="rounded-circle img-thumbnail"
+                  src={profile?.song_art_image_url}
+                  style={{ height: "300px", width: "300px" }}
+                />
+                <div class="artist-box-content">
+                  <h3 class="title">{profile?.full_title}</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ color: "whitesmoke" }}
+            className="container  text-center pt-3"
+          >
+            <div className="row">
+              <div className="col">
+                <p>{profile?.description?.dom.children[0].children[0]}</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <strong>Recording location</strong>
+                <p>{profile?.recording_location}</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <strong>Release date</strong>
+                <p>{profile?.release_date_for_display}</p>
+              </div>
+            </div>
+            <div className="row ">
+              <div className="col d-flex justify-content-center pb-3">
+                <ReactPlayer playing={true} width="100%" url={element} />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Loading />
+        </>
+      )}
+    </>
+  );
+}
